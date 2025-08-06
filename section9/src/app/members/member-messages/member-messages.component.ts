f8509251525040d8449d25fa77938affb7935026
Khoin06 <ngokhoinsl@gmail.com>
2025-08-06 02:09:38 -0700

    Import section9 into /section9

---
+++ section9/src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Member } from 'src/app/_models/member';
+import { Message } from 'src/app/_models/message';
+import { MessageService } from 'src/app/_services/message.service';
+
+
+
+@Component({
+  selector: 'app-member-messages',
+  templateUrl: './member-messages.component.html',
+  styleUrls: ['./member-messages.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MemberMessagesComponent implements OnInit {
+  
+   @ViewChild('messageForm'	) messageForm: NgForm;
+  @Input() messages: Message[]; 
+   @Input() username: string;
+   messageContent: string;
+
+  constructor(public messagesService: MessageService ) { }
+
+  ngOnInit(): void {  
+
+  }
+
+  sendMessage(){
+    this.messagesService.sendMessage(this.username, this.messageContent).then(() => {
+      this.messageForm.reset();
+    })
+  }
+
+}
