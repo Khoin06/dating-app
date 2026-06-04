cfc0969bb4d967e8f9c8b234cf976a811f02c581
khoin06 <ngokhoinsl@gmail.com>
2026-06-04 09:25:33 +0700

    config: update api url to new Render service dating-app-api-39h5

--- client/src/app/environments/environment.prod.ts
+++ client/src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://dating-app-nce0.onrender.com/api/',
-    hubUrl: 'https://dating-app-nce0.onrender.com/hubs/'
+    apiUrl: 'https://dating-app-api-39h5.onrender.com/api/',
+    hubUrl: 'https://dating-app-api-39h5.onrender.com/hubs/'
 };
